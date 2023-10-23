ed7c0e152420e8830ae2cc7f2760b04f83a1c944
Rishabh Anand <ranand16@gmail.com>
2023-10-23 08:19:08 +0530

    bun rest a[i

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+    .get("/", () => "Hello Rishabh Anand")
+    .get("/post/:id", ({ params: { id } }) => {
+        return { id: id, title: " Learning bun" };
+    })
+    .listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
